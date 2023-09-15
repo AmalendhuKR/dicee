29619992cc47d436f6ad2f3c1a558cb69b04878c
Amalendhu K R <amalendhu@dexlock.com>
2023-09-15 14:31:58 +0530

    course ended

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,26 +16,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDice(diceName) {
+    // change both dices
+    if (diceName == 'left')
+      leftDiceNumber = Random().nextInt(6) + 1;
+    else if (diceName == 'right') rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    var diceNumber = 5;
     return Center(
       child: Row(
         children: [
           Expanded(
             child: TextButton(
-              child: Image.asset("images/dice$diceNumber.png"),
+              child: Image.asset("images/dice$leftDiceNumber.png"),
               onPressed: () {
-                print("Left button pressed");
+                setState(() {
+                  changeDice('left');
+                });
               },
             ),
           ),
           Expanded(
             child: TextButton(
-              child: Image.asset("images/dice$diceNumber.png"),
+              child: Image.asset("images/dice$rightDiceNumber.png"),
               onPressed: () {
-                print("Right button pressed");
+                setState(() {
+                  changeDice('right');
+                });
               },
             ),
           )
